8fe88bfbabb0b6f2908247d01fe6d5e8f431831a
sktarsk <117266861+sktarsk@users.noreply.github.com>
2024-04-02 18:22:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,7 +64,7 @@ HOWTOVERIFY = environ.get('HOWTOVERIFY', 'https://t.me/TeamLeech') # How to open
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.paisakamalo.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'ed4f17106956cea1ea9f258507c17e2501b701aa')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))  # else--> True
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))  # else--> True
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
