9e65735b06849f35287c0a05ed6bcf3463b2b968
sktarsk <117266861+sktarsk@users.noreply.github.com>
2024-04-04 10:40:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1001878965765') # support gro
 reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001780805995') # request channel id ( make sure bot is admin )
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
-NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want no results messages in Log Channel
+NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True)) # True if you want no results messages in Log Channel
 
 # MongoDB information 
 # https://youtu.be/qFB0cFqiyOM?si=QGuFSZ7qhxl4VTrA
