e6ad3403292bf8b2ea01e6f6c4aec8619f22cfab
sktarsk <117266861+sktarsk@users.noreply.github.com>
2024-05-30 12:48:07 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -472,13 +472,13 @@ async def get_shortlink(chat_id, link):
     if URL.startswith("shorturllink") or URL.startswith("terabox.in") or URL.startswith("urlshorten.in"):
         URL = SHORTLINK_URL
         API = SHORTLINK_API
-    if URL == "api.shareus.io":
+    if URL == "paisakamalo.in":
         # method 1:
         # https = link.split(":")[0] #splitting https or http from link
         # if "http" == https: #if https == "http":
         #     https = "https"
         #     link = link.replace("http", https) #replacing http to https
-        # conn = http.client.HTTPSConnection("api.shareus.io")
+        # conn = http.client.HTTPSConnection("paisakamalo.in")
         # payload = json.dumps({
         #   "api_key": "2Os2Dk6GnNhV88jLAINnahwZ4d13",
         #   "monetization": True,
@@ -540,7 +540,7 @@ async def get_verify_shorted_link(link):
         https = "https"
         link = link.replace("http", https)
 
-    if URL == "api.shareus.io":
+    if URL == "paisakamalo.in":
         url = f"https://{URL}/shortLink"
         params = {"token": API,
                   "format": "json",
