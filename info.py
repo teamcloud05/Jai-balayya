48108f6d87d2c14ac77010511e38919e1019322e
teamcloud05 <107703880+teamcloud05@users.noreply.github.com>
2024-07-23 16:24:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,8 +47,8 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True)) # True if you want no
 # MongoDB information 
 # https://youtu.be/qFB0cFqiyOM?si=QGuFSZ7qhxl4VTrA
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://deepak1256:deepak1256@cluster0.pepxsdt.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "deepak1256")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://deepak2245:deepak1234@cluster0.8fstnoo.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Verify/token system
